d9171ac9c8670f6129d909221d3acf6c849e4631
gatsuBerserk <berserkdev@outlook.it>
2022-02-21 15:29:08 +0100

    password generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-document.writeln("Hello World")
+console.log("I notice that you are very curious");
+alert("Welcome to my first iteration with JS"); 
+
+const firstName= prompt (" Ciao! Qual'è il tuo nome?"); 
+document.getElementById("name").innerHTML= firstName; 
+
+const last= prompt ("E il tuo cognome?"); 
+document.getElementById("lastName").innerHTML= last; 
+
+const color= prompt (" Qual'è il tuo colore preferito?"); 
+document.getElementById("favoriteColor").innerHTML= color; 
+
+let passwordGenerator= firstName + last + color;
+document.getElementById("password").innerHTML= passwordGenerator;
