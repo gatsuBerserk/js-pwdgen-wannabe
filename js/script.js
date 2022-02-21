1ef4e45a46a807fb4296b92269546a86837630a2
gatsuBerserk <berserkdev@outlook.it>
2022-02-21 16:01:57 +0100

    add comments in JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 console.log("I notice that you are very curious");
 alert("Welcome to my first iteration with JS"); 
 
+// Input Name
 const firstName= prompt (" Ciao! Qual'è il tuo nome?"); 
 document.getElementById("name").innerHTML= firstName; 
 
+// Input LastName
 const last= prompt ("E il tuo cognome?"); 
 document.getElementById("lastName").innerHTML= last; 
 
+// Input Preferit Color
 const color= prompt (" Qual'è il tuo colore preferito?"); 
 document.getElementById("favoriteColor").innerHTML= color; 
 
-let passwordGenerator= firstName + last + color;
-document.getElementById("password").innerHTML= passwordGenerator;
+// Password Generator
+let passwordGenerator= "//&&_" + firstName + last + color + "21" + "_%$$";
+document.getElementById("password").innerHTML= passwordGenerator; 
+
+// Console
+console.log(
+	`	
+		${firstName}: Nome,
+		${last}: Cognome,
+		${color}:Colore preferito, 
+        ${passwordGenerator}: Password generata  
+	`
+);
